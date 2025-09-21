c406b4017018f6d63c151cb3fe719f79bc9f18a9
Onorde <onordsahin@proton.me>
2025-09-21 14:47:22 +0300

    Minor Option Fixes
    Removed unused UI elements

--- options.gd
+++ options.gd
@@ -12,6 +12,12 @@ func _on_mute_toggled(toggled_on: bool) -> void:
 	AudioServer.set_bus_mute(0,toggled_on)
 
 
+
+func _on_return_pressed() -> void:
+	ChangeScene.change_scene("res://main_menu.tscn")
+
+
+
 func _on_resolutions_item_selected(index: int) -> void:
 	match index:
 		0:
@@ -28,15 +34,3 @@ func _on_window_modes_item_selected(index: int) -> void:
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 		1:
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
-
-func _on_return_pressed() -> void:
-	ChangeScene.change_scene("res://main_menu.tscn")
-
-
-
-func _on_font_types_item_selected(index: int) -> void:
-	pass # Replace with function body.
-
-
-func _on_font_size_slider_value_changed(value: float) -> void:
-	pass # Replace with function body.
